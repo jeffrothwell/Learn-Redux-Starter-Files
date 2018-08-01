e5ca8f463d2031f521feacd80ed56564aada3a45
jeffrothwell <rothwelljeff@gmail.com>
2018-08-01 08:52:53 -0400

    render the Photo component within single

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Photo from './Photo'
+// import comments
 
 class Single extends React.Component {
   render() {
+    // need index of post
+    // need the post itself
+
+    const i = this.props.posts.findIndex(post => post.code === this.props.params.postId)
+    const post = this.props.posts[i]
+
     return (
       <div className="single-photo">
-        I'm the single
+        <Photo i={i} post={post} {...this.props} />
       </div>
     )
   }
