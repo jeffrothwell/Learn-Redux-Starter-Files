8f4076578c8fabca48e28a6fb3bcc0671a7671b3
jeffrothwell <rothwelljeff@gmail.com>
2018-08-01 09:47:46 -0400

    render comments in the single photo component

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import Photo from './Photo'
-// import comments
+import Comments from './Comments'
 
 class Single extends React.Component {
   render() {
-    // need index of post
-    // need the post itself
-
-    const i = this.props.posts.findIndex(post => post.code === this.props.params.postId)
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId)
     const post = this.props.posts[i]
 
     return (
       <div className="single-photo">
         <Photo i={i} post={post} {...this.props} />
+        <Comments />
       </div>
     )
   }
